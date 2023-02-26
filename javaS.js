4805ea5f9a1a7035513a5ab8602ee067df58272a
faiz033033 <feju302@gmail.com>
2023-02-26 12:52:26 +0530

    first commit

---
+++ javaS.js
@@ -0,0 +1,8 @@
+function test() {
+    var uid = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+
+var user = localStorage.setItem("uid",uid);
+var em = localStorage.setItem("email",email);
+
+}
