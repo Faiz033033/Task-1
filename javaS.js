d308d0809105f64759420144107092b9a6ccfddc
faiz033033 <feju302@gmail.com>
2023-02-27 08:34:03 +0530

    multiple user and printing

--- javaS.js
+++ javaS.js
@@ -1,11 +1,32 @@
-function test() {
-    var uid = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
+function storeData() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
 
-var user = localStorage.setItem(uid,email);
+    const userData = JSON.parse(localStorage.getItem('userData')) || [];
 
-localStorage.getItem(uid,email);
-
-
-
+    userData.push({ name, email, phone });
+  
+    localStorage.setItem('userData', JSON.stringify(userData));
+ 
+    displayData();
 }
+
+function displayData() {
+  
+    const userData = JSON.parse(localStorage.getItem('userData')) || [];
+  
+    const userList = document.getElementById('user-data');
+  
+    userList.innerHTML = '';
+    
+    userData.forEach(user => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${user.name} - ${user.email} - ${user.phone}`;
+      userList.appendChild(listItem);
+    });
+}
+  
+  
+  displayData();
+  
