943bd93e292d4c988ed45dc201bb2c3162a8f540
faiz033033 <feju302@gmail.com>
2023-03-02 23:09:18 +0530

    Adding Delete Button

--- javaS.js
+++ javaS.js
@@ -1,31 +1,29 @@
-function storeData() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-
-    const userData = JSON.parse(localStorage.getItem('userData')) || [];
-
-    userData.push({ name, email, phone });
-  
-    localStorage.setItem('userData', JSON.stringify(userData));
- 
-    displayData();
-}
-
 function displayData() {
-  
     const userData = JSON.parse(localStorage.getItem('userData')) || [];
   
     const userList = document.getElementById('user-data');
   
     userList.innerHTML = '';
-    
-    userData.forEach(user => {
+
+    userData.forEach((user, index) => {
       const listItem = document.createElement('li');
       listItem.textContent = `${user.name} - ${user.email} - ${user.phone}`;
+  
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', () => {
+        // Remove the user data item from the userData array
+        userData.splice(index, 1);
+
+        localStorage.setItem('userData', JSON.stringify(userData));
+        displayData();
+      });
+  
+      listItem.appendChild(deleteButton);
+
       userList.appendChild(listItem);
     });
-}
+  }
   
   
   displayData();
